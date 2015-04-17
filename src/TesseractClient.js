aa26cf8e5b7f10a11c5670941899c8a980aaa0bd
Elliot Chance <elliot@kounta.com>
2015-04-17 17:20:52 +1000

    Clean up code

--- src/TesseractClient.js
+++ src/TesseractClient.js
@@ -2,38 +2,37 @@
 // established `socket` is passed in so we can communicate with the server from
 // here on out.
 function TesseractClient(socket) {
-	this.socket = socket;
+  this.socket = socket;
 }
 
 // Fetch the result of a SQL statement.
-TesseractClient.prototype.fetch = function (sql, callback) {
-	
-	// The SQL has to be packaged up in an object and sent as JSON to the
-	// server.
-	var message = {
-		'sql': sql
-	};
-	this.socket.write(JSON.stringify(message));
+TesseractClient.prototype.fetch = function(sql, callback) {
+  // The SQL has to be packaged up in an object and sent as JSON to the
+  // server.
+  var message = {
+    'sql': sql
+  };
+  this.socket.write(JSON.stringify(message));
 
-	// When the server responds it will respond in JSON (for success or error).
-	// We simply decode it and throw it back to the callback.
-	this.socket.on('data', function (result) {
-		callback(JSON.parse(result));
-	});
+  // When the server responds it will respond in JSON (for success or error).
+  // We simply decode it and throw it back to the callback.
+  this.socket.on('data', function(result) {
+    callback(JSON.parse(result));
+  });
 };
 
 // Close (destroy) the client connection. It's always polite to close the
 // connection when your finished with it, although there is no requirement to do
 // so.
-TesseractClient.prototype.close = function () {
-	this.socket.destroy();
+TesseractClient.prototype.close = function() {
+  this.socket.destroy();
 };
 
 // Insert a new record. This is actually just a shorthand for writing the full
 // SQL.
-TesseractClient.prototype.insert = function (tableName, object, callback) {
-	var sql = 'INSERT INTO ' + tableName + ' ' + JSON.stringify(object);
-	this.fetch(sql, callback);
+TesseractClient.prototype.insert = function(tableName, object, callback) {
+  var sql = 'INSERT INTO ' + tableName + ' ' + JSON.stringify(object);
+  this.fetch(sql, callback);
 };
 
 module.exports = TesseractClient;
--- test/client.js
+++ test/client.js
@@ -2,82 +2,82 @@ var tesseract = require('../src/Tesseract.js');
 
 // Here are the general tests for the client.
 
-exports.testConnect = function (test) {
-    tesseract.connect(null, function (err, client) {
-        test.equals(err, null);
-        test.done();
+exports.testConnect = function(test) {
+  tesseract.connect(null, function(err, client) {
+    test.equals(err, null);
+    test.done();
 
-        client.close();
-    });
+    client.close();
+  });
 };
 
-exports.testSimpleQuery = function (test) {
-    tesseract.connect(null, function (err, client) {
-        test.equals(err, null);
+exports.testSimpleQuery = function(test) {
+  tesseract.connect(null, function(err, client) {
+    test.equals(err, null);
 
-        client.fetch('SELECT 1 + 2', function (result) {
-            test.ok(result.success);
-            test.equals(result.data[0].col1, 3);
-            test.done();
+    client.fetch('SELECT 1 + 2', function(result) {
+      test.ok(result.success);
+      test.equals(result.data[0].col1, 3);
+      test.done();
 
-            client.close();
-        });
+      client.close();
     });
+  });
 };
 
-exports.testQueryError = function (test) {
-    tesseract.connect(null, function (err, client) {
-        test.equals(err, null);
+exports.testQueryError = function(test) {
+  tesseract.connect(null, function(err, client) {
+    test.equals(err, null);
 
-        client.fetch('SELECT "abc" + 1', function (result) {
-            test.equals(result.success, false);
-            test.equals(result.error, 'No such operator string + number.');
-            test.done();
+    client.fetch('SELECT "abc" + 1', function(result) {
+      test.equals(result.success, false);
+      test.equals(result.error, 'No such operator string + number.');
+      test.done();
 
-            client.close();
-        });
+      client.close();
     });
+  });
 };
 
-exports.testMultipleQueries = function (test) {
-    var queries = 10;
-    var received = 0;
+exports.testMultipleQueries = function(test) {
+  var queries = 10;
+  var received = 0;
 
-    function processResult(result, client) {
-        test.ok(result.success);
-        test.equals(result.data[0].col1, 3);
+  function processResult(result, client) {
+    test.ok(result.success);
+    test.equals(result.data[0].col1, 3);
 
-        ++received;
-        if (received == queries) {
-            test.done();
-            client.close();
-        }
+    ++received;
+    if (received == queries) {
+      test.done();
+      client.close();
+    }
+  }
+
+  tesseract.connect(null, function(err, client) {
+    test.equals(err, null);
+
+    function callback(result) {
+      processResult(result, client);
     }
 
-    tesseract.connect(null, function (err, client) {
-        test.equals(err, null);
-
-        function callback(result) {
-            processResult(result, client);
-        }
-
-        // Send off a bunch of queries
-        for (var i = 0; i < queries; ++i) {
-            client.fetch('SELECT 1 + 2', callback);
-        }
-    });
+    // Send off a bunch of queries
+    for (var i = 0; i < queries; ++i) {
+      client.fetch('SELECT 1 + 2', callback);
+    }
+  });
 };
 
-exports.testDefaultPort = function (test) {
-    test.equals(tesseract.defaultPort, 3679);
+exports.testDefaultPort = function(test) {
+  test.equals(tesseract.defaultPort, 3679);
+  test.done();
+};
+
+exports.testCallingCloseOnAnAlreadyClosedConnection = function(test) {
+  tesseract.connect(null, function(err, client) {
+    test.equals(err, null);
     test.done();
-};
-
-exports.testCallingCloseOnAnAlreadyClosedConnection = function (test) {
-    tesseract.connect(null, function (err, client) {
-        test.equals(err, null);
-        test.done();
-        client.close();
-        client.close();
-    });
+    client.close();
+    client.close();
+  });
 };
